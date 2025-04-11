25162210930cbcde9f3f41ad2f6e94bb983ea8e8
Aljaž Ferenc <aljaz.ferenc@specto.si>
2025-04-11 13:14:55 +0200

    get logs by user api

---
+++ src/app/api/v1/logs/[userId]/route.ts
@@ -0,0 +1,31 @@
+import {NextRequest, NextResponse} from "next/server";
+import MesocycleModel from "@/database/models/Mesocycle.model";
+import WorkoutLogModel from "@/database/models/WorkoutLog.model";
+
+export async function OPTIONS() {
+    return NextResponse.json({}, {
+        status: 200,
+        headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "GET, POST, OPTIONS, DELETE",
+            "Access-Control-Allow-Headers": "Content-Type, Authorization",
+        },
+    });
+}
+
+export async function GET(request: NextRequest, {params}: {params: Promise<{userId: string}>}){
+    try{
+        const {userId} = await params
+        const logs = await WorkoutLogModel.find({createdBy: userId})
+
+        return NextResponse.json({data: logs}, {
+            headers: {
+                "Access-Control-Allow-Origin": "*",
+            },
+            status: 200
+        })
+    }catch(err){
+        console.log(err)
+        return NextResponse.json({error: 'No mesoId'}, {status: 404})
+    }
+}
