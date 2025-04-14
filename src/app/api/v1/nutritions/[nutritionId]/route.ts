d0ad959c440d05ef4a34a12b602905590e3df14e
Aljaž Ferenc <aljaz.ferenc@specto.si>
2025-04-14 12:22:28 +0200

    create and delete nutrition api

---
+++ src/app/api/v1/nutritions/[nutritionId]/route.ts
@@ -0,0 +1,35 @@
+import {NextRequest, NextResponse} from "next/server";
+import MesocycleModel from "@/database/models/Mesocycle.model";
+import {connectToDatabase} from "@/database/mongoose";
+import NutritionModel from "@/database/models/Nutrition.model";
+
+export async function OPTIONS() {
+    return NextResponse.json({}, {
+        status: 200,
+        headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "GET, POST, OPTIONS, DELETE, PUT",
+            "Access-Control-Allow-Headers": "Content-Type, Authorization",
+        },
+    });
+}
+
+
+export async function DELETE(request: NextRequest, {params}: { params: Promise<{ nutritionId: string }> }) {
+    try {
+        await connectToDatabase()
+        const {nutritionId} = await params
+        console.log(nutritionId)
+        const deletedNutrition = await NutritionModel.findByIdAndDelete(nutritionId)
+
+        return NextResponse.json(deletedNutrition, {
+            headers: {
+                "Access-Control-Allow-Origin": "*",
+            },
+            status: 200
+        })
+    } catch (err) {
+        console.log(err)
+        return NextResponse.json({error: 'No mesoId'}, {status: 404})
+    }
+}
--- src/app/api/v1/nutritions/route.ts
+++ src/app/api/v1/nutritions/route.ts
@@ -1,4 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
+import {connectToDatabase} from "@/database/mongoose";
+import NutritionModel from "@/database/models/Nutrition.model";
 
 export async function OPTIONS() {
     return NextResponse.json({}, {
@@ -15,10 +17,10 @@ export async function POST(request: NextRequest) {
     try {
         const nutrition = await request.json();
         console.log(nutrition)
-        // await connectToDatabase();
-        // await NutritionModel.create(nutrition)
+        await connectToDatabase();
+        const newNutrition = await NutritionModel.create(nutrition)
 
-        return NextResponse.json(nutrition, {
+        return NextResponse.json(newNutrition, {
             status: 201,
             headers: { "Access-Control-Allow-Origin": "*" }
         });
