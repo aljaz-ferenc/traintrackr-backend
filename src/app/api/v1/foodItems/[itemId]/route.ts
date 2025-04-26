fdc92d160e6f640bc1ec0deebaa351c882857afd
aljaz-ferenc <aljaz.ferenc@protonmail.com>
2025-04-26 20:20:34 +0200

    delete food item api

--- src/app/api/v1/foodItems/[itemId]/route.ts
+++ src/app/api/v1/foodItems/[itemId]/route.ts
@@ -9,7 +9,7 @@ export async function OPTIONS() {
             status: 200,
             headers: {
                 "Access-Control-Allow-Origin": "*",
-                "Access-Control-Allow-Methods": "GET, POST, OPTIONS, PUT",
+                "Access-Control-Allow-Methods": "GET, POST, OPTIONS, PUT, DELETE",
                 "Access-Control-Allow-Headers": "Content-Type, Authorization",
             },
         },
@@ -24,14 +24,14 @@ export async function PUT(
         await connectToDatabase();
         const {itemId} = await params;
         const item = await request.json();
-        console.log(itemId)
+
         const updatedItem = await FoodItemModel.findOneAndReplace(
             {_id: itemId},
             item,
         );
 
         return NextResponse.json(
-            null,
+            updatedItem,
             {
                 status: 200,
                 headers: {
@@ -41,6 +41,34 @@ export async function PUT(
         );
     } catch (err) {
         console.error("Error updating mesocycle:", err);
+        return NextResponse.json(
+            {message: "Internal Server Error"},
+            {status: 500},
+        );
+    }
+}
+
+export async function DELETE(
+    request: NextRequest,
+    {params}: { params: Promise<{ itemId: string }> },
+) {
+    try {
+        await connectToDatabase();
+        const {itemId} = await params;
+
+        const deletedItem = await FoodItemModel.findByIdAndDelete(itemId)
+
+        return NextResponse.json(
+            deletedItem,
+            {
+                status: 200,
+                headers: {
+                    "Access-Control-Allow-Origin": "*",
+                },
+            },
+        );
+    } catch (err) {
+        console.error("Error deleting food item:", err);
         return NextResponse.json(
             {message: "Internal Server Error"},
             {status: 500},
